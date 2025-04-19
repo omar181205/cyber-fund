6131de7768827994e3b68dba682c68d1cf9d11a4
omar <oa2300487@tkh.edu.eg>
2025-04-19 20:20:42 +0200

    wrote the code

--- code cyber fund.py
+++ code cyber fund.py
@@ -1,2 +1,13 @@
-print("hello")
-vtgvt
+import os
+
+def collect_files(directory):
+    for root, dirs ,files in os.walk(directory):
+        for file in files:
+            if file.endswith((".txt", ".jpg", ".docx")):
+                print(os.path.join(root, file))
+
+try:
+    path = input("spectify the path to use: ")
+    collect_files(path)
+except FileNotFoundError:
+    print("file does not exist")    
